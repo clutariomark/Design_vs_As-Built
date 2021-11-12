d8d78168cb6f48e5dc5735c78037dda6a97a7538
hiroshi <hiroshiy@scanx.com>
2021-11-12 23:59:30 +0900

    add api server script

---
+++ main_api.py
@@ -0,0 +1,26 @@
+from typing import Optional
+from fastapi import FastAPI
+from subprocess import *
+import os
+
+app = FastAPI()
+
+@app.get("/dvab/")
+def calc(las: Optional[str] = None, xml: Optional[str] = None ,c: Optional[str] = None, m: Optional[str] = None, a: Optional[str] = None, c_fill: Optional[str] = None):
+    # subprocess.call(['C:\\Users\\kjohn\\Desktop\\Design_vs_AsBuilt_Beta\\test.bat {} {} {} {} {}'.format(las, xml, c, m, a)])
+    # output = 'C:\\Users\\kjohn\\Desktop\\Design_vs_AsBuilt_Beta\\test.bat {}'.format(las)
+    # subprocess.call([output])
+    os.system(f"command.bat {las} {xml} {c} {m} {a} {c_fill}")
+    return {'las': las,
+            'xml': xml,
+            'construction': c,
+            'method': m,
+            'area': a,
+            'cell_fill': c_fill}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, c: Optional[str] = None, m: Optional[str] = None, a: Optional[str] = None):
+    return {"item_id": item_id, "construction": c, "method": m, "area": a}
+
+# "C:\Program Files\ArcGIS\Pro\bin\Python\Scripts\propy.bat" "C:\Users\kjohn\Documents\GitHub\Design_vs_As-Built\design_vs_as_built.py" 
+# "C:\Users\kjohn\Desktop\Design_vs_AsBuilt_Beta\OGI_Infotec\dekigata.las" "C:\Users\kjohn\Desktop\Design_vs_AsBuilt_Beta\OGI_Infotec\design.xml" riverbed fill slope MINIMUM
